4ea4115cd9fccd327107ad78d19988cac3cf52e6
PODDAe <camalkaakash2@gmail.com>
2025-07-13 17:56:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=k0Ay1IxB#uLfnDi2fqIJ7u8oAc2XqDZeE8OEkBaNahAhuhQCvZDY",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=U1RAUJoL#_qxlvBNfFH4bH7BegtbhT4IbR5ToQF71_FyZ94WSzdc",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
